4ab4ad8c627330ff0344d7c2fcaaa6bfef89c9af
MManzotti <mateusmanzotti@hotmail.com>
2021-05-13 00:29:54 +0000

    subindo trabalho

--- exemplo_vm/provider.tf
+++ exemplo_vm/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "Mateus-impacta-Turma04 "
+  project = "mateus-impacta-turma04"
   region = "us-central1"
   zone = "us-central1-a"
   credentials = "../../mateus-impacta-turma04.json"
